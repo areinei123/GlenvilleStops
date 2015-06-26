7505499bc03339c047ffee6a3804f5d1612182ed
Fred Chapman <areinei123@gmail.com>
2015-06-26 11:31:00 -0400

    Added CMS for easy content management

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  
+  ComfortableMexicanSofa::Routing.admin(:path => '/cms-admin')
+  
+  # Make sure this routeset is defined last
+  ComfortableMexicanSofa::Routing.content(:path => '/', :sitemap => false)
   root 'homes#index'
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
